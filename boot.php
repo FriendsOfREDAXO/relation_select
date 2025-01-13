a1f812353f94c132d6c25cb951c87b7cf8303346
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-13 15:44:26 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,7 +1,8 @@
 <?php
 if (rex::isBackend() && rex_be_controller::getCurrentPage() != 'login') {
-    rex_view::addJsFile('https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/js/tom-select.complete.min.js');
-    rex_view::addCssFile('https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.default.min.css');
+    rex_view::addJsFile('https://cdn.jsdelivr.net/npm/choices.js@10.2.0/public/assets/scripts/choices.min.js');
+    rex_view::addCssFile('https://cdn.jsdelivr.net/npm/choices.js@10.2.0/public/assets/styles/choices.min.css');
+    rex_view::addJsFile('https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js');
     rex_view::addJsFile(rex_url::addonAssets('relation_select', 'relation_select.js'));
     rex_view::addCssFile(rex_url::addonAssets('relation_select', 'relation_select.css'));
 }
