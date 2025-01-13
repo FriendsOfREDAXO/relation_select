6e4b332c72b72638859b0df507a6e8cf89eab8b8
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-13 15:41:28 +0100

    Update relation_select.js

--- assets/relation_select.js
+++ assets/relation_select.js
@@ -1,4 +1,13 @@
 $(document).on('rex:ready', function() {
+    // Destroy existing instances
+    $('input[data-relation-config]').each(function() {
+        const select = this.previousElementSibling;
+        if (select && select._tomSelect) {
+            select._tomSelect.destroy();
+        }
+    });
+
+    // Initialize new instances
     $('input[data-relation-config]').each(function() {
         const input = this;
         const config = JSON.parse(input.dataset.relationConfig || '{}');
@@ -12,41 +21,42 @@ $(document).on('rex:ready', function() {
             valueField: 'value',
             labelField: 'label',
             searchField: 'label',
-            plugins: ['remove_button', 'drag_drop'],
+            plugins: {
+                remove_button: {},
+                drag_drop: {
+                    onDrop: function() {
+                        setTimeout(() => {
+                            input.value = ts.getValue().join(',');
+                            $(input).trigger('change');
+                        }, 10);
+                    }
+                }
+            },
             placeholder: config.placeholder || 'Bitte wählen...',
             load: function(query, callback) {
                 fetch(`index.php?rex-api-call=relation_select&table=${config.table}&value_field=${config.valueField}&label_field=${config.labelField}`)
                     .then(response => response.json())
-                    .then(json => {
-                        callback(json);
-                    }).catch(() => {
-                        callback();
-                    });
+                    .then(callback).catch(() => callback());
             },
             onInitialize: function() {
                 const self = this;
                 const values = input.value.split(',').filter(v => v);
                 if (values.length) {
-                    self.load(function(callback) {
-                        fetch(`index.php?rex-api-call=relation_select&table=${config.table}&value_field=${config.valueField}&label_field=${config.labelField}`)
-                            .then(response => response.json())
-                            .then(json => {
-                                values.forEach(value => {
-                                    const item = json.find(i => i.value.toString() === value);
-                                    if (item) self.addOption(item);
-                                });
-                                values.forEach(value => self.addItem(value));
-                                callback();
-                            });
+                    self.load(function(json) {
+                        values.forEach(value => {
+                            const item = json.find(i => i.value.toString() === value.toString());
+                            if (item) {
+                                self.addOption(item);
+                                self.addItem(value);
+                            }
+                        });
                     });
                 }
+            },
+            onChange: function(value) {
+                input.value = Array.isArray(value) ? value.join(',') : value;
+                $(input).trigger('change');
             }
-        });
-
-        // Event Handler für Änderungen
-        select.addEventListener('change', function() {
-            const values = ts.getValue();
-            input.value = Array.isArray(values) ? values.join(',') : values;
         });
     });
 });
