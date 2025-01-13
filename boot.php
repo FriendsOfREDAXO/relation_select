1011c63069b80c1341c53f79e950abd973b6a4eb
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-13 18:15:02 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,5 +1,6 @@
 <?php
 if (rex::isBackend() && rex_be_controller::getCurrentPage() != 'login') {
+    rex_view::addJsFile('https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js');
     rex_view::addJsFile(rex_url::addonAssets('relation_select', 'relation_select.js'));
     rex_view::addCssFile(rex_url::addonAssets('relation_select', 'relation_select.css'));
 }
