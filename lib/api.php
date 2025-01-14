dff6179f8eeb5b48fba6cf754dbc737389c7598e
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-14 10:13:18 +0100

    Update api.php

--- lib/api.php
+++ lib/api.php
@@ -1,11 +1,67 @@
-<?php 
+<?php
 class rex_api_relation_select extends rex_api_function
 {
     protected $published = true;
 
     function execute()
     {
-        // ... [vorheriger Code bleibt gleich] ...
+        $table = rex_get('table', 'string');
+        $valueField = rex_get('value_field', 'string');
+        $labelField = rex_get('label_field', 'string');
+        $dbWhere = rex_get('dbw', 'string', '');
+        $dbOrderBy = rex_get('dboy', 'string', '');
+
+        if (!$table || !$valueField || !$labelField) {
+            throw new rex_api_exception('Missing parameters');
+        }
+
+        $sql = rex_sql::factory();
+        
+        // Parse WHERE conditions
+        $where = [];
+        $params = [];
+        if ($dbWhere) {
+            $conditions = array_map('trim', explode(',', $dbWhere));
+            foreach ($conditions as $condition) {
+                $parsedCondition = $this->parseCondition($condition);
+                if ($parsedCondition) {
+                    $where[] = $parsedCondition['sql'];
+                    if (isset($parsedCondition['value'])) {
+                        $params[] = $parsedCondition['value'];
+                    }
+                }
+            }
+        }
+        
+        // Parse ORDER BY
+        $orderClauses = [];
+        if ($dbOrderBy) {
+            $orders = array_map('trim', explode(',', $dbOrderBy));
+            for ($i = 0; $i < count($orders); $i += 2) {
+                $field = $orders[$i];
+                $direction = isset($orders[$i + 1]) ? strtoupper($orders[$i + 1]) : 'ASC';
+                if ($direction !== 'DESC') $direction = 'ASC';
+                
+                $orderClauses[] = $sql->escapeIdentifier($field) . ' ' . $direction;
+            }
+        }
+        
+        // Build query
+        $query = "SELECT $valueField as value, $labelField as label FROM $table";
+        if (!empty($where)) {
+            $query .= ' WHERE ' . implode(' AND ', $where);
+        }
+        if (!empty($orderClauses)) {
+            $query .= ' ORDER BY ' . implode(', ', $orderClauses);
+        } else {
+            $query .= " ORDER BY $labelField";
+        }
+        
+        $options = $sql->getArray($query, $params);
+
+        header('Content-Type: application/json');
+        echo json_encode($options);
+        exit;
     }
     
     private function parseCondition($condition) 
